5adb4964fdbc888dd331b037116c5bd4b9745857
ash <97464181+Borgerr@users.noreply.github.com>
2024-02-03 19:31:12 -0700

    empty input, I/O redirection

--- src/shell/command.rs
+++ src/shell/command.rs
@@ -4,6 +4,7 @@ use rustix::{
     io::Errno,
 }; // https://docs.rs/rustix/latest/rustix/
 
+#[derive(Debug)]
 pub enum ShellCmd<'a> {
     Exec {
         args: &'a [String],
@@ -17,25 +18,29 @@ pub enum ShellCmd<'a> {
         left: Box<ShellCmd<'a>>,
         right: Box<ShellCmd<'a>>,
     },
+    Nothing,
 }
 
 impl<'a> ShellCmd<'a> {
     /// Recursively creates a shell command.
     /// Should initially be envoked with a simple slice of some vector of arguments.
     pub fn new(args: &'a [String]) -> Result<Self, Errno> {
-        // TODO: ensure file descriptors are created correctly in redir variants
+        // check for silly empty args
+        if args == [""] {
+            return Ok(Self::Nothing);
+        }
 
         // find special symbols,
         // right now includes `|`, `>`, and `<`
-        let mut args_iterator = args.into_iter();
-        if let Some(first_pipe) = args_iterator.position(|x| x == "|") {
+        let args_iterator = args.into_iter();
+        if let Some(first_pipe) = args_iterator.clone().position(|x| x == "|") {
             // Pipes have lowest precedence in current version
             // create and return a Pipe variant
             Ok(Self::Pipe {
                 left: Box::new(Self::new(&args[0..first_pipe])?),
                 right: Box::new(Self::new(&args[first_pipe + 1..args.len()])?),
             })
-        } else if let Some(first_larrow) = args_iterator.position(|x| x == "<") {
+        } else if let Some(first_larrow) = args_iterator.clone().position(|x| x == "<") {
             // create and return a Redir variant with readmode on,
             // files are always on the rhs
             Ok(Self::Redir {
@@ -43,7 +48,7 @@ impl<'a> ShellCmd<'a> {
                 descriptor: open(args[first_larrow + 1].clone(), OFlags::RDONLY, Mode::RUSR)?,
                 readmode: true,
             })
-        } else if let Some(first_rarrow) = args_iterator.position(|x| x == ">") {
+        } else if let Some(first_rarrow) = args_iterator.clone().position(|x| x == ">") {
             // create and return a Redir variant with readmode off,
             // files are always on the rhs
             Ok(Self::Redir {
--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -1,4 +1,8 @@
 use fork::{fork, Fork}; // https://docs.rs/fork/0.1.18/fork/
+use rustix::{
+    fd::OwnedFd,
+    stdio::{dup2_stdin, dup2_stdout},
+};
 use std::{io::stdin, process::exit};
 
 mod command;
@@ -8,31 +12,36 @@ use command::ShellCmd;
 /// - get input from user
 /// - spawn child to execute user command
 pub fn sh_loop() {
-    let mut input = String::new();
     let stdin = stdin();
 
     loop {
+        let mut input = String::new();
         if let Err(error) = stdin.read_line(&mut input) {
             println!("error when reading input (Error: {error})");
         }
         input.pop(); // get rid of newline
         let args_vec: Vec<String> = input.split(" ").map(|x| String::from(x)).collect();
         match ShellCmd::new(&args_vec[0..args_vec.len()]) {
-            Ok(command) => match fork() {
-                Ok(Fork::Parent(_)) => (),
-                Ok(Fork::Child) => sh_launch(command),
-                Err(error) => println!("error when forking shell process (error: {error})"),
-            },
+            Ok(command) => {
+                if let ShellCmd::Nothing = command {
+                    continue;
+                }
+                match fork() {
+                    Ok(Fork::Parent(_)) => (),
+                    Ok(Fork::Child) => sh_launch(command),
+                    Err(error) => println!("error when forking shell process (error: {error})"),
+                }
+            }
 
             Err(errno) => println!("error when parsing command (Errno: {errno}"),
-        }
+        };
     }
 }
 
 /// Launching a command from a child process.
 /// Process exits or gives control to a different process after executing this function.
-fn sh_launch(command: ShellCmd) {
-    match command {
+fn sh_launch(cmd: ShellCmd) {
+    match cmd {
         ShellCmd::Exec { args } => {
             let err = exec::execvp(args[0].clone(), args); // only returns if there's an error
             println!("Error executing program {}:\n{}", args[0], err);
@@ -41,9 +50,27 @@ fn sh_launch(command: ShellCmd) {
             command,
             descriptor,
             readmode,
-        } => todo!(),
+        } => launch_redir(*command, descriptor, readmode),
         ShellCmd::Pipe { left, right } => todo!(),
+        ShellCmd::Nothing => (), // empty input
     }
 
     exit(0)
+}
+
+fn launch_redir(command: ShellCmd, descriptor: OwnedFd, readmode: bool) {
+    if readmode {
+        // replace stdin file descriptor with `descriptor`
+        if let Err(errno) = dup2_stdin(descriptor) {
+            println!("error when changing file descriptors (Errno: {errno}");
+            exit(-1);
+        }
+    } else {
+        // replace stdout file descriptor with `descriptor`
+        if let Err(errno) = dup2_stdout(descriptor) {
+            println!("error when changing file descriptors (Errno: {errno})");
+            exit(-1);
+        }
+    }
+    sh_launch(command);
 }
